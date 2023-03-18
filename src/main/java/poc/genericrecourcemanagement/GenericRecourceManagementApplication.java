7625463b4f2e369d005ea45faa48bc94827b34d8
Harry Chan <hei1233212000@gmail.com>
2023-03-18 18:52:01 +0800

    init

---
+++ src/main/java/poc/genericrecourcemanagement/GenericRecourceManagementApplication.java
@@ -0,0 +1,13 @@
+package poc.genericrecourcemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenericRecourceManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GenericRecourceManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/poc/genericrecourcemanagement/GenericRecourceManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package poc.genericrecourcemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenericRecourceManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
