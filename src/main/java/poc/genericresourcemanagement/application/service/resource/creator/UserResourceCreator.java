a16793f6900344e11fa38f20e66cf84882d9098f
Harry Chan <hei1233212000@gmail.com>
2023-05-01 11:04:41 +0800

    minor refactoring

--- src/main/java/poc/genericresourcemanagement/application/service/resource/creator/UserResourceCreator.java
+++ src/main/java/poc/genericresourcemanagement/application/service/resource/creator/UserResourceCreator.java
@@ -3,7 +3,6 @@ package poc.genericresourcemanagement.application.service.resource.creator;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.log4j.Log4j2;
 import poc.genericresourcemanagement.application.service.common.UserComponent;
-import poc.genericresourcemanagement.domain.model.ResourceType;
 import poc.genericresourcemanagement.infrastructure.persistence.model.UserPersistenceEntity;
 import poc.genericresourcemanagement.infrastructure.persistence.repository.UserRepository;
 import reactor.core.publisher.Mono;
